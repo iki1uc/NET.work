9e3ad7fc3c2807b394b13bddae18930397be3fe1
iki1uc <entertrainer@web.de>
2026-09-08 20:36:39 +0200

    Update vec.9.js

--- user/vec.9.js
+++ user/vec.9.js
@@ -1,16 +1,25 @@
-export const VEC_9x9x9 = {
-    height: 9,
-    width: 9,
-    depth: 9,
-    slots: 9 * 9 * 9,   // 729
-    matrix: []
-};
+export function makeNET_TMP(){
+    return {
+        fritz: {
+            box: "router",
+            id: crypto.randomUUID(),   // anonym, nicht rückverfolgbar
+            down: 690,
+            up: 86.3
+        },
 
-// Matrix erzeugen
-for(let h=0; h<9; h++){
-    for(let w=0; w<9; w++){
-        for(let d=0; d<9; d++){
-            VEC_9x9x9.matrix.push({ h, w, d });
-        }
-    }
+        lan: [
+            { id: crypto.randomUUID(), speed: 1000 },
+            { id: crypto.randomUUID(), speed: 1000 },
+            { id: crypto.randomUUID(), speed: 1000 }
+        ],
+
+        wlan: [
+            { id: crypto.randomUUID(), speed: 864 },
+            { id: crypto.randomUUID(), speed: 540 }
+        ],
+
+        vpn: [
+            { id: crypto.randomUUID(), type: "IPSec" }
+        ]
+    };
 }

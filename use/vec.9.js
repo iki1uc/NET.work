49904e357cdf73c5afe2b3e2d1ffaf8693b6f095
iki1uc <entertrainer@web.de>
2026-09-08 21:24:30 +0200

    Update vec.9.js

--- use/vec.9.js
+++ use/vec.9.js
@@ -2,11 +2,10 @@ export const VEC_9x9x9 = {
     height: 9,
     width: 9,
     depth: 9,
-    slots: 9 * 9 * 9,   // 729
+    slots: 729,
     matrix: []
 };
 
-// Matrix erzeugen
 for(let h=0; h<9; h++){
     for(let w=0; w<9; w++){
         for(let d=0; d<9; d++){
